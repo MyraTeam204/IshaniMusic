e2cbcb0efa303202be80bfe2b5fe75f23575ef3a
MyraTeam204 <131482282+MyraTeam204@users.noreply.github.com>
2023-06-06 18:06:30 +0530

    Update inline.py

--- AnonX/plugins/inline.py
+++ AnonX/plugins/inline.py
@@ -5,7 +5,6 @@ from youtubesearchpython.__future__ import VideosSearch
 
 from config import BANNED_USERS, MUSIC_BOT_NAME
 from AnonX import app
-from AnonX.utils.inlinequery import answer
 
 
 @app.on_inline_query(~BANNED_USERS)
